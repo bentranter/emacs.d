df28a9e24bac8ad749317dfdc5193afddd9054ef
Ben Tranter <bwtrante@lakeheadu.ca>
2016-06-27 23:23:39 -0400

    Put current theme under version control

---
+++ themes/spacegray-theme.el
@@ -0,0 +1,9 @@
+;;; spacegray-theme.el --- Dark theme Spacegray for Sublime.
+
+(require 'spacegray-definitions
+         (locate-file "spacegray-definitions.el" custom-theme-load-path
+                      '("c" "")))
+
+(create-spacegray-theme)
+
+;;; spacegray-theme.el ends here
